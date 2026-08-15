0b6d6bc118f0cef5a47a7d6592c5a234d42a0a12
GirlDadMula11-sudo <GirlDadMula11@icloud.com>
2026-08-15 19:58:54 -0400

    Add Georgie native iOS application entry point

---
+++ ios/GeorgieApp/GeorgieApp.swift
@@ -0,0 +1,32 @@
+import SwiftUI
+import AppIntents
+
+@main
+struct GeorgieApp: App {
+    @StateObject private var store = AssistantStore()
+    @Environment(\.scenePhase) private var scenePhase
+
+    init() {
+        GeorgieShortcuts.updateAppShortcutParameters()
+    }
+
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+                .environmentObject(store)
+                .preferredColorScheme(.dark)
+                .onOpenURL { url in
+                    Task { await store.handleDeepLink(url) }
+                }
+                .task {
+                    await NotificationManager.shared.requestAuthorization()
+                    await store.refreshDashboard()
+                }
+        }
+        .onChange(of: scenePhase) { _, phase in
+            if phase == .active {
+                Task { await store.refreshDashboard() }
+            }
+        }
+    }
+}
