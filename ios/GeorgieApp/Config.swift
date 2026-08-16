d7ff352a7f0a60f5470165811fde49766379e676
GirlDadMula11-sudo <GirlDadMula11@icloud.com>
2026-08-16 09:13:13 -0400

    Secure Georgie device identity in iPhone Keychain

--- ios/GeorgieApp/Config.swift
+++ ios/GeorgieApp/Config.swift
@@ -3,15 +3,19 @@ import Security
 
 enum GeorgieConfig {
     static let baseURL = URL(string: "https://georgie-kappa.vercel.app")!
-    static let userHeader = "X-Georgie-User"
     static let sessionHeader = "X-Georgie-Session"
     static let deviceTokenKey = "com.sierramarketinginc.georgie.device-token"
     static let deviceIDKey = "com.sierramarketinginc.georgie.device-id"
 
     static var deviceID: String {
-        if let value = UserDefaults.standard.string(forKey: deviceIDKey) { return value }
+        if let value = KeychainStore.read(account: deviceIDKey) { return value }
+        if let legacy = UserDefaults.standard.string(forKey: deviceIDKey), !legacy.isEmpty {
+            KeychainStore.save(legacy, account: deviceIDKey)
+            UserDefaults.standard.removeObject(forKey: deviceIDKey)
+            return legacy
+        }
         let value = UUID().uuidString
-        UserDefaults.standard.set(value, forKey: deviceIDKey)
+        KeychainStore.save(value, account: deviceIDKey)
         return value
     }
 
@@ -27,8 +31,10 @@ enum GeorgieConfig {
 enum KeychainStore {
     static func save(_ value: String, account: String) {
         let data = Data(value.utf8)
-        let query: [String: Any] = [kSecClass as String: kSecClassGenericPassword,
-                                    kSecAttrAccount as String: account]
+        let query: [String: Any] = [
+            kSecClass as String: kSecClassGenericPassword,
+            kSecAttrAccount as String: account
+        ]
         SecItemDelete(query as CFDictionary)
         var item = query
         item[kSecValueData as String] = data
@@ -37,13 +43,23 @@ enum KeychainStore {
     }
 
     static func read(account: String) -> String? {
-        let query: [String: Any] = [kSecClass as String: kSecClassGenericPassword,
-                                    kSecAttrAccount as String: account,
-                                    kSecReturnData as String: true,
-                                    kSecMatchLimit as String: kSecMatchLimitOne]
+        let query: [String: Any] = [
+            kSecClass as String: kSecClassGenericPassword,
+            kSecAttrAccount as String: account,
+            kSecReturnData as String: true,
+            kSecMatchLimit as String: kSecMatchLimitOne
+        ]
         var result: AnyObject?
         guard SecItemCopyMatching(query as CFDictionary, &result) == errSecSuccess,
               let data = result as? Data else { return nil }
         return String(data: data, encoding: .utf8)
+    }
+
+    static func delete(account: String) {
+        let query: [String: Any] = [
+            kSecClass as String: kSecClassGenericPassword,
+            kSecAttrAccount as String: account
+        ]
+        SecItemDelete(query as CFDictionary)
     }
 }
