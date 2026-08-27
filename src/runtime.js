ccea0729fc71defaf549f48bad906bed28787ef3
GirlDadMula11-sudo <GirlDadMula11@icloud.com>
2026-08-26 21:51:09 -0400

    route startup through canonical runtime registry

--- src/runtime.js
+++ src/runtime.js
@@ -1,29 +1,5 @@
 import "dotenv/config";
 import "./server.js";
-import { startCloudStateRecovery } from "./cloud-state.js";
-import { startApprovalDispatchWorker } from "./tools.js";
-import { startEngineeringCoordinator } from "./engineering-coordinator.js";
-import { startMaintenanceSentinel } from "./maintenance-sentinel.js";
-import { startReconciliationWorkers } from "./reconciliation-workers.js";
-import { startBackgroundOperatingLayer } from "./background-operating-layer.js";
-import { startSelfEvolution } from "./self-evolution.js";
-import { startObjectiveWorker } from "./objective-worker.js";
-import { startSeoMonitorScheduler } from "./seo-monitor.js";
-import { startSmartleadReplyCloserWorker } from "./smartlead-reply-closer-worker.js";
-import { startLenderDeliveryWorker } from "./lender-delivery-worker.js";
-import { startSierraClosingOutreachWorker } from "./sierra-closing-outreach-worker.js";
+import { startRuntimeProfile } from "./runtime-components.js";
 
-startCloudStateRecovery();
-startApprovalDispatchWorker();
-startMaintenanceSentinel();
-startReconciliationWorkers();
-startSelfEvolution();
-startBackgroundOperatingLayer();
-startEngineeringCoordinator();
-startObjectiveWorker();
-startSeoMonitorScheduler();
-startSmartleadReplyCloserWorker();
-startLenderDeliveryWorker();
-startSierraClosingOutreachWorker();
-
-console.log("Georgie unified server + durable background runtime online");
+startRuntimeProfile("web");
