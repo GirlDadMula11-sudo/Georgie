141f6303afdb03b870dfbf8e20ca0b13b4ce4edc
GirlDadMula11-sudo <GirlDadMula11@icloud.com>
2026-08-27 08:04:38 -0400

    Phase 2: start Georgie core before specialists

--- src/runtime.js
+++ src/runtime.js
@@ -1,5 +1,6 @@
 import "dotenv/config";
 import "./server.js";
-import { startRuntimeProfile } from "./runtime-components.js";
+import { scheduleRuntimePlane, startRuntimeProfile } from "./runtime-components.js";
 
-startRuntimeProfile("web");
+startRuntimeProfile("web", { plane: "core" });
+scheduleRuntimePlane("web", "specialist");
